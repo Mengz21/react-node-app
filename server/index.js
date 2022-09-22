72d13d6ef180da4c38db1dc9131a6720bd9116f0
Mengfan Zhang <mfzhang@berkeley.edu>
2022-09-22 16:03:15 -0600

    Add heroku configuration variables, procfile, gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // server/index.js
 const path = require('path');
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,12 +10,12 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+mongoose.connect("mongodb+srv://hulo9:MONGODB_KEY@cluster0.ihunb0h.mongodb.net/blogDB", {useNewUrlParser : true});
+
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
-
 
 
 
